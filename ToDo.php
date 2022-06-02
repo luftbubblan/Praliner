2637130b2a3b0af5466514790acfededf058f53b
Oscar Blomberg <oscarblomberg@hotmail.com>
2022-06-02 13:33:09 +0200

    Todolist updated

--- ToDo.php
+++ ToDo.php
@@ -1,43 +1,36 @@
-<?php
-/**
- Todo
- 
- Bygga databasen
+Bygga databasen
 
- dbconnection.php
- Footer.php
- Header.php
+dbconnection.php
+Footer.php
+Header.php
 
- Börja med produktdelen - Admin
+Börja med produktdelen - Admin
     Lista alla produkterna
     Skapa nya produkter
     Deleta produkter
     Uppdatera produkter
  
-    Skapa validering av alla fälten
+Skapa validering av alla fälten
  
  
- produktdelen - Shop
+produktdelen - Shop
     Skapa huvudmeny mellan Admin och shop
     Lista alla produkterna - Home page
     Kunna klicka på enskild prdukt och visa den
  
  
- Användardelen - Admin
+Användardelen - Admin
     Skapa meny för användardelen/produktdelen
     Lista alla användare
     Skapa nya användare
     Deleta användare
     Uppdatera användare
 
- Användardelen - Shop
+Användardelen - Shop
     Tar vi senare
  
- Varukorg - Lär vi oss sen
+Varukorg - Lär vi oss sen
  
- Kassasidan - Lär vi oss sen
+Kassasidan - Lär vi oss sen
  
- Hantera ordrar - Tar vi senare
- 
- */
-?>
+Hantera ordrar - Tar vi senare
