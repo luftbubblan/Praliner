2d26885945cec90b39deace0eb2119558b28884a
Oscar Blomberg <oscarblomberg@hotmail.com>
2022-06-02 13:31:42 +0200

    Some bases done

--- public/admin/index.php
+++ public/admin/index.php
@@ -1,9 +1,19 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Admin</title>
-</head>
-<body>
-	<h1>Admin page</h1>
-</body>
-</html>
+<?php
+	$pageTitle = "Admin page";
+
+	include('layout/header.php');
+?>
+
+
+
+
+
+<h1>Admin page</h1>
+<a href="http://localhost/Praliner/public">Shop</a>
+
+
+
+
+
+
+<?php include('layouts/footer.php') ?>
--- public/index.php
+++ public/index.php
@@ -1,9 +1,14 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Shop</title>
-</head>
-<body>
-	<h1>Shop page</h1>
-</body>
-</html>
+<?php
+	$pageTitle = "Shop page";
+
+	include('layout/header.php');
+?>
+
+
+<h1>Shop page</h1>
+<a href="http://localhost/Praliner/public/admin">Admin</a>
+
+	
+
+
+<?php include('layouts/footer.php') ?>
