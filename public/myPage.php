1d0ef0a1c7cd083e0bb9e0d2360083b7b66723a4
Oscar Blomberg <oscarblomberg@hotmail.com>
2022-06-06 20:54:11 +0200

    Dynamic welcome on mypage

--- public/myPage.php
+++ public/myPage.php
@@ -8,21 +8,21 @@
         header('Location: login.php?mustLogin');
     }
 
-    echo "<pre>";
-    print_r($_POST);
-    echo "</pre>";
-
+    
     $sql = "
-            SELECT * FROM users
-            WHERE id = :id
-        ";
-
-        $stmt = $pdo->prepare($sql);
-        $stmt->bindParam(':id', $_SESSION['id']);
-        $stmt->execute();
-        $user = $stmt->fetch();
-
-
+        SELECT * FROM users
+        WHERE id = :id
+    ";
+    
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':id', $_SESSION['id']);
+    $stmt->execute();
+    $user = $stmt->fetch();
+    
+    
+    echo "<pre>";
+    print_r($user);
+    echo "</pre>";
 
 	include('layout/header.php');
 ?>
@@ -30,6 +30,8 @@
 
 <h1>My Page</h1>
 <a href="index.php">Shop</a>
+
+<p>Welcome <?=$user['first_name']?></p>
 
 
 <?php include('layouts/footer.php') ?>
