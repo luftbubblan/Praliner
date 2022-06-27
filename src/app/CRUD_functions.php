92a4effb7a476437ee4483edbd466f3d6b48ecb5
Oscar Blomberg <oscarblomberg@hotmail.com>
2022-06-27 19:43:52 +0200

    refactoring done

--- src/app/CRUD_functions.php
+++ src/app/CRUD_functions.php
@@ -446,6 +446,80 @@ class CRUDFunctions {
         header('Location: index.php?deleted');
         exit;
     }
+
+    function createOrder($message, $userId, $guest, $fullName, $email, $phone, $street, $postalCode, $city, $country) {
+        if(empty($message)) {
+            $sql = "
+                INSERT INTO orders (
+                    user_id,
+                    total_price,
+                    guest,
+                    billing_full_name,
+                    email,
+                    phone,
+                    billing_street,
+                    billing_postal_code,
+                    billing_city,
+                    billing_country)
+                VALUES (
+                    :user_id,
+                    :total_price,
+                    :guest,
+                    :billing_full_name,
+                    :email,
+                    :phone,
+                    :billing_street,
+                    :billing_postal_code,
+                    :billing_city,
+                    :billing_country)
+            ";
+                
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->bindParam(':user_id', $userId);
+            $stmt->bindParam(':total_price', $_POST['totalSum']);
+            $stmt->bindParam(':guest', $guest);
+            $stmt->bindParam(':billing_full_name', $fullName);
+            $stmt->bindParam(':email', $email);
+            $stmt->bindParam(':phone', $phone);
+            $stmt->bindParam(':billing_street', $street);
+            $stmt->bindParam(':billing_postal_code', $postalCode);
+            $stmt->bindParam(':billing_city', $city);
+            $stmt->bindParam(':billing_country', $country);
+            $stmt->execute();
+            $orderId = $this->pdo->lastInsertId();
+
+            foreach($_SESSION['cartItems'] as $item) {
+                $sql = "
+                    INSERT INTO order_items (
+                        order_id,
+                        product_id,
+                        product_title,
+                        quantity,
+                        unit_price)
+                    VALUES (
+                        :order_id,
+                        :product_id,
+                        :product_title,
+                        :quantity,
+                        :unit_price)
+                "; 
+                $stmt = $this->pdo->prepare($sql);
+                $stmt->bindParam(':order_id', $orderId);
+                $stmt->bindParam(':product_id', $item['id']);
+                $stmt->bindParam(':product_title', $item['title']);
+                $stmt->bindParam(':quantity', $item['quantity']);
+                $stmt->bindParam(':unit_price', $item['price']);
+                $stmt->execute();
+            }
+
+            $_SESSION['orderId'] = $orderId;
+            header('Location: order-confirmation.php');
+            exit;
+        } else {
+            header('Location: checkout.php?message=' . $message);
+            exit;
+        }
+    }
 }
 
 $crudFunctions = new CRUDFunctions($pdo);
