9f4ae7e75a212f3cdf83bc992d16b5694b45891f
Oscar Blomberg <oscarblomberg@hotmail.com>
2022-06-28 10:55:40 +0200

    code indented

--- public/admin/index.php
+++ public/admin/index.php
@@ -29,60 +29,60 @@
 ?>
 
 <div class="content-products">
-<h2>Admin</h2>
-<h4>Hantera produkter</h4>
+	<h2>Admin</h2>
+	<h4>Hantera produkter</h4>
 
-<form action="addNewProduct.php">
-	<input type="submit" class="add-new-btn btn btn-success" value="Lägg till ny produkt">
-</form>
+	<form action="addNewProduct.php">
+		<input type="submit" class="add-new-btn btn btn-success" value="Lägg till ny produkt">
+	</form>
 
-<?=$message?>
+	<?=$message?>
 
-<table id="admin-tbl">
-	<thead>
-		<tr id="product-list">
-			<th>Namn</th>
-			<th>Smak</th>
-			<th>Beskrivning</th>
-			<th>Pris</th>
-			<th>Lagerstatus</th>
-			<th>Bild</th>
-			<th></th>
-		</tr>
-	</thead>
-
-    <tbody>
-	    <?php foreach($products as $product) { ?>
-			<tr>
-				<td><?=htmlentities($product['title'])?></td>
-				<td><?=htmlentities($product['flavour'])?></td>
-				<td><?=substr(htmlentities($product['description']), 0, 170)?></td>
-				<td><?=htmlentities($product['price'])?></td>      
-				<td><?=htmlentities($product['stock'])?></td>
-				<td><?=htmlentities($product['img_url'])?></td>
-				<td class="table-btns" >
-                    <!-- Edit -->
-                    <form action="edit.php" method="POST">
-                        <input type="hidden" name="id" value="<?=$product['id'] ?>">
-                        <input type="hidden" name="title" value="<?=$product['title'] ?>">
-                        <input type="hidden" name="flavour" value="<?=$product['flavour'] ?>">
-                        <input type="hidden" name="description" value="<?=$product['description'] ?>">
-                        <input type="hidden" name="price" value="<?=$product['price'] ?>">
-                        <input type="hidden" name="stock" value="<?=$product['stock'] ?>">
-                        <input type="hidden" name="img_url" value="<?=$product['img_url'] ?>">
-                        <input type="submit" id="edit-btn" class="edit-btn btn btn-success" value="Redigera">
-                    </form>
-
-                    <!-- Delete -->
-                    <form action="" method="POST">
-                        <input type="hidden" name="id" value="<?=$product['id'] ?>">
-                        <input type="submit" class="btn btn-danger" name="deleteProductBtn" value="Radera">
-                    </form>
-                </td>
+	<table id="admin-tbl">
+		<thead>
+			<tr id="product-list">
+				<th>Namn</th>
+				<th>Smak</th>
+				<th>Beskrivning</th>
+				<th>Pris</th>
+				<th>Lagerstatus</th>
+				<th>Bild</th>
+				<th></th>
 			</tr>
-        <?php } ?>
-	</tbody>
-</table>
+		</thead>
+
+		<tbody>
+			<?php foreach($products as $product) { ?>
+				<tr>
+					<td><?=htmlentities($product['title'])?></td>
+					<td><?=htmlentities($product['flavour'])?></td>
+					<td><?=substr(htmlentities($product['description']), 0, 170)?></td>
+					<td><?=htmlentities($product['price'])?></td>      
+					<td><?=htmlentities($product['stock'])?></td>
+					<td><?=htmlentities($product['img_url'])?></td>
+					<td class="table-btns" >
+						<!-- Edit -->
+						<form action="edit.php" method="POST">
+							<input type="hidden" name="id" value="<?=$product['id'] ?>">
+							<input type="hidden" name="title" value="<?=$product['title'] ?>">
+							<input type="hidden" name="flavour" value="<?=$product['flavour'] ?>">
+							<input type="hidden" name="description" value="<?=$product['description'] ?>">
+							<input type="hidden" name="price" value="<?=$product['price'] ?>">
+							<input type="hidden" name="stock" value="<?=$product['stock'] ?>">
+							<input type="hidden" name="img_url" value="<?=$product['img_url'] ?>">
+							<input type="submit" id="edit-btn" class="edit-btn btn btn-success" value="Redigera">
+						</form>
+
+						<!-- Delete -->
+						<form action="" method="POST">
+							<input type="hidden" name="id" value="<?=$product['id'] ?>">
+							<input type="submit" class="btn btn-danger" name="deleteProductBtn" value="Radera">
+						</form>
+					</td>
+				</tr>
+			<?php } ?>
+		</tbody>
+	</table>
 </div>
 
 
